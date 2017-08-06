fe84f08bab2bd69c6d9efe8b6a64e0a6cf66e172
rae1519 <andersondurasia@gmail.com>
2017-08-05 23:25:44 -0400

    Update Rotate.py

--- Rotate.py
+++ Rotate.py
@@ -2,18 +2,25 @@ import math
 import matplotlib.pyplot as plt
 
 def rotate(a, b, d_angle):
+    # converts angle to radians
     theta = math.radians(d_angle)
     
+    #difference between x coordinates
     dx = b[0] - a[0]
+    # difference between y coordinates
     dy = b[1] - a[1]
     
+    # add original x coordiante to x result from rotation matrix
     new_x = a[0] + ((dx*math.cos(theta) - dy*math.sin(theta)))
+    # add original x coordiante to x result from rotation matrix
     new_y = a[1] + ((dx*math.sin(theta) + dy*math.cos(theta)))
     return new_x, new_y
-
+# test point 1
 one = (5, 5)
 
+# test point 2
 two = (0, 0)
+
 
 hope = rotate(one, two, 90)
 plt.plot(one, two, 'green')
